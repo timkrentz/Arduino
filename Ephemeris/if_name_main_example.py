9ce2689db3d06db5d77b089d5daebf90d7705170
timkrentz <tskrentz@gmail.com>
2015-06-19 14:37:49 -0400

    Added python test code and if __name__ == "main" notes

---
+++ Ephemeris/if_name_main_example.py
@@ -0,0 +1,39 @@
+#Beginning of file
+#should really only contain function definitions,
+#this file is intended to be "imported"
+
+#Function 1
+
+#Function 2
+
+
+
+
+if __name__ == "__main__"
+	#Execute these functions
+
+	#This is a testbed environment just for this package
+	#Declare testbed data and variables, then call the
+	#above functions to produce a test of this package
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
--- Ephemeris/test.py
+++ Ephemeris/test.py
@@ -2,6 +2,23 @@
 #PyEphem is used for ephemeris calculations
 #The goal is to use this in designing the Sun Sensor
 
-import pyephem
+import ephem
+
+#U = ephem.Uranus()
+#print('%s' % (U.name))
+
+cmu = ephem.Observer()
+cmu.lat = 40.2634501
+cmu.lon = -79.5642191
+cmu.elevation = 289
+cmu.date = '2015/6/19 18:51:00'
+
+S = ephem.Sun(cmu)
+print('%s %s' % (S.az, S.alt))
+
+
+
+
+
 
 
