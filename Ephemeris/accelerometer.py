088788dcf64997ce995a69362926bb54ed4480e7
timkrentz <tskrentz@gmail.com>
2015-06-19 17:13:44 -0400

    Added package for connecting to Arduino Accelerometer

---
+++ Ephemeris/accelerometer.py
@@ -0,0 +1,40 @@
+# Package for reading Acceleromter->Arduino setup
+# Author: Tim Krentz
+# Created Friday, June 16, 2015
+
+import serial
+
+
+
+#Accelerometer creation function, creates and returns serial object
+#Required: port name and buadrate
+#assumes 8 bits, no parity, 1 stop bit
+#Further dev: Allow creation without params (use defaults)
+def create(port='/dev/ttyACM0', baudrate=115200):
+	return serial.Serial(port, baudrate)
+
+def read(num=1):
+	data = []
+	while (num >0):
+		data.append(self.readline())
+		num -= 1
+	return data
+
+
+
+
+
+
+
+
+
+#Testbench code, runs top-level
+if __name__ == "__main__":
+	
+	#Create accelerometer object
+	accel =	create('/dev/ttyACM0', 115200)
+
+	print(accel.read())
+	print('Running...')
+
+
