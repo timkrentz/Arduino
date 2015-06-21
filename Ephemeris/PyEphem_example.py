ebf3086bfa5cb2d3098e948b0973b9dfaf69967b
timkrentz <tskrentz@gmail.com>
2015-06-21 18:34:49 -0400

    Added example of PyEphem usage

---
+++ Ephemeris/PyEphem_example.py
@@ -0,0 +1,26 @@
+#Experimental Code for learning to use the PyEphem library
+#PyEphem is used for ephemeris calculations
+#The goal is to use this in designing the Sun Sensor
+# Tim Krentz
+# June 21, 2015
+
+
+import ephem
+
+
+cmu = ephem.Observer()
+cmu.lat = 40.2634501
+cmu.lon = -79.5642191
+cmu.elevation = 289
+cmu.date = '2015/6/19 18:51:00'
+
+
+S = ephem.Sun(cmu)
+print('%s %s' % (S.az, S.alt))
+
+
+
+
+
+
+
